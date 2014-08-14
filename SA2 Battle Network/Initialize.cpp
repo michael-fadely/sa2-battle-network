8b11ee2e932901fbcea00164d40d61181548285c
SonicFreak94 <sonicfreak94@hotmail.com>
2014-08-13 21:50:18 -0600

    Brackets on same line as statement = blasphemy

--- SA2 Battle Network/Initialize.cpp
+++ SA2 Battle Network/Initialize.cpp
@@ -27,10 +27,12 @@ wchar_t** argv = nullptr;
 
 void __cdecl Init_t(const char *path)
 {
-	if (setvbuf(stdout, 0, _IOLBF, 4096) != 0) {
+	if (setvbuf(stdout, 0, _IOLBF, 4096) != 0)
+	{
 		abort();
 	}
-	if (setvbuf(stderr, 0, _IOLBF, 4096) != 0) {
+	if (setvbuf(stderr, 0, _IOLBF, 4096) != 0)
+	{
 		abort();
 	}
 
