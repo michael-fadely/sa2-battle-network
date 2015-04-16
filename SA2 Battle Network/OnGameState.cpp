2fe55c30704b1f7bbae48d0ebbe9019633297c9f
SonicFreak94 <sonicfreak94@hotmail.com>
2015-04-16 01:59:09 -0600

    This works except for the part where it breaks gamestate changes after the stage restarts.

--- SA2 Battle Network/OnGameState.cpp
+++ SA2 Battle Network/OnGameState.cpp
@@ -1,19 +1,20 @@
+#include <vector>
+
+#include <LazyTypedefs.h>
 #include <SA2ModLoader.h>	// for everything
-#include "Networking.h"		// for MSG
-#include "Globals.h"		// for Globals :specialed:
+#include "CommonEnums.h"	// for GameState enum
+#include "AddressList.h"	// for GameState, FrameCount
+//#include "Globals.h"		// for Globals :specialed:
 
 #include "OnGameState.h"
 
-DataPointer(uint8, byte_93BC5A, 0x93BC5A);
-
-void* escape_addr = (void*)0x0043AB1E;
+void* escape_addr = (void*)0x0043AAD2;
 
 void InitOnGameState()
 {
-	// Adding nops first because the existing instruction is 6 bytes,
-	// but the call we're adding is 5 bytes.
-	uint8 patch[6] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
-	WriteData(escape_addr, arrayptrandlength(patch));
+	// Adding nops first because the existing instruction smaller than a call.
+	std::vector<uint8> patch(7, 0x90);
+	WriteData(escape_addr, patch.data(), patch.size());
 	WriteCall(escape_addr, OnGameState);
 }
 
@@ -21,11 +22,11 @@ static void __cdecl OnGameState()
 {
 	// This is here because we overwrite its assignment with a call
 	// in the original code.
-	byte_93BC5A = 0;
+	GameState = GameState::INGAME;
 	OnGameState_Ingame();
 }
 
 void OnGameState_Ingame()
 {
-	PrintDebug("INCOMING LAG");
+	PrintDebug("\a[%06d] Stage loaded.", FrameCount);
 }
