df3d76942456275438fa57b01a360ffd74b8eabb
SonicFreak94 <sonicfreak94@hotmail.com>
2016-01-29 12:15:16 -0700

    Fancy

--- SA2 Battle Network/PacketOverloads.cpp
+++ SA2 Battle Network/PacketOverloads.cpp
@@ -13,23 +13,3 @@ sf::Packet& operator >>(sf::Packet& packet, char& data)
 {
 	return packet >> (signed char&)data;
 }
-
-// TODO: template
-sf::Packet& operator<<(sf::Packet& packet, const std::vector<char>& data)
-{
-	packet << (sf::Uint32)data.size();
-	packet.append(data.data(), data.size());
-	return packet;
-}
-
-sf::Packet& operator>>(sf::Packet& packet, std::vector<char>& data)
-{
-	sf::Uint32 length;
-	packet >> length;
-	data.resize(length);
-
-	for (sf::Uint32 i = 0; i < length; i++)
-		packet >> data[i];
-
-	return packet;
-}
--- SA2 Battle Network/PacketOverloads.h
+++ SA2 Battle Network/PacketOverloads.h
@@ -6,6 +6,24 @@
 sf::Packet& operator <<(sf::Packet& packet, const char& data);
 sf::Packet& operator >>(sf::Packet& packet, char& data);
 
-sf::Packet& operator <<(sf::Packet& packet, const std::vector<char>& data);
-sf::Packet& operator >>(sf::Packet& packet, std::vector<char>& data);
+template <typename T>
+sf::Packet& operator<<(sf::Packet& packet, const std::vector<T>& data)
+{
+	packet << (sf::Uint32)data.size();
+	for (sf::Uint32 i = 0; i < data.size(); i++)
+		packet << data[i];
+	return packet;
+}
 
+template <typename T>
+sf::Packet& operator>>(sf::Packet& packet, std::vector<T>& data)
+{
+	sf::Uint32 length;
+	packet >> length;
+	data.resize(length);
+
+	for (sf::Uint32 i = 0; i < length; i++)
+		packet >> data[i];
+
+	return packet;
+}
