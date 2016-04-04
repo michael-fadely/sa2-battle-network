b2989379d15e4f5380214c5d14199f684c3bbad0
SonicFreak94 <sonicfreak94@hotmail.com>
2016-04-04 01:09:18 -0600

    Fixed #23

--- SA2 Battle Network/PoseEffect2PStartMan.cpp
+++ SA2 Battle Network/PoseEffect2PStartMan.cpp
@@ -11,6 +11,8 @@ void __cdecl PoseEffect2PStartMan_Delete(ObjectMaster* obj)
 	{
 		Broker->SendReady(MessageID::S_RoundStart);
 		Broker->WaitForPlayers(MessageID::S_RoundStart);
+		MainCharObj1[0]->Action = 0;
+		MainCharObj1[1]->Action = 0;
 	}
 }
 
