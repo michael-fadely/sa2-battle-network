f95aa782ffa3de4b5f8b2dd62cc24c3112cc79d2
Michael Fadely <michaelwfadely@gmail.com>
2018-03-10 20:08:49 -0700

    upgrade compiler

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 git submodule update --init --recursive
 cd SFML
-cmake -DBUILD_SHARED_LIBS:BOOL="0" -G "Visual Studio 14 2015" -T "v140_xp"
+cmake -DBUILD_SHARED_LIBS:BOOL="0" -G "Visual Studio 15 2017" -T "v141_xp"
 cd ..
