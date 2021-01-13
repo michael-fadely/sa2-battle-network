bbff53f0f4edb5d8e11c2e56d080e49a7d019402
Michael Fadely <michaelwfadely@gmail.com>
2021-01-12 19:10:10 -0800

    clean up mod.cpp

--- sa2-battle-network/mod.cpp
+++ sa2-battle-network/mod.cpp
@@ -20,51 +20,55 @@ void fake_main(const char* path, int argc, wchar_t** argv);
 
 extern "C"
 {
-	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer, nullptr, nullptr, 0, nullptr, 0, nullptr, 0, nullptr, 0 };
+__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer, nullptr, nullptr, 0, nullptr, 0, nullptr, 0, nullptr, 0 };
 
-	__declspec(dllexport) void __cdecl Init(const char* path)
-	{
-		int argc       = 0;
-		wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-		fake_main(path, argc, argv);
-		LocalFree(argv);
-	}
+__declspec(dllexport) void __cdecl Init(const char* path)
+{
+	int argc = 0;
+	wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	fake_main(path, argc, argv);
+	LocalFree(argv);
+}
 }
 
-std::string build_mod_path(const char* modpath, const char* path)
+std::string build_mod_path(const char* mod_path, const char* path)
 {
 	std::stringstream result;
-	char workingdir[FILENAME_MAX];
+	char working_dir[FILENAME_MAX] {};
 
-	result << _getcwd(workingdir, FILENAME_MAX) << "\\" << modpath << "\\" << path;
+	result << _getcwd(working_dir, FILENAME_MAX) << "\\" << mod_path << "\\" << path;
 
 	return result.str();
 }
 
 void parse_config(const std::string& path, Program::Settings& settings, sws::Address& address)
 {
-	char buffer[255] {};
+	std::array<char, 255> buffer {};
 
-	GetPrivateProfileStringA("Config", "Nickname", "", buffer, 255, path.c_str());
+	GetPrivateProfileStringA("Config", "Nickname", "", buffer.data(), buffer.size(), path.c_str());
 
-	if (strlen(buffer))
+	if (buffer[0] != '\0')
 	{
-		settings.nickname = buffer;
+		settings.nickname = buffer.data();
 	}
 
 	settings.no_specials = GetPrivateProfileIntA("Config", "Specials", 1, path.c_str()) == 0;
-	settings.cheats      = GetPrivateProfileIntA("Config", "Cheats", 0, path.c_str()) != 0;
+	settings.cheats = GetPrivateProfileIntA("Config", "Cheats", 0, path.c_str()) != 0;
 
-	GetPrivateProfileStringA("Server", "Name", "", buffer, 255, path.c_str());
-	if (strlen(buffer))
-		settings.server_name = buffer;
+	GetPrivateProfileStringA("Server", "Name", "", buffer.data(), buffer.size(), path.c_str());
+
+	if (buffer[0] != '\0')
+	{
+		settings.server_name = buffer.data();
+	}
 
 	address.port = static_cast<sws::port_t>(GetPrivateProfileIntA("Server", "Port", 21790, path.c_str()));
 
-	GetPrivateProfileStringA("Server", "Password", "", buffer, 255, path.c_str());
-	if (strlen(buffer))
+	GetPrivateProfileStringA("Server", "Password", "", buffer.data(), buffer.size(), path.c_str());
+
+	if (buffer[0] != '\0')
 	{
-		std::string password_a(buffer);
+		const std::string password_a(buffer.data(), buffer.size());
 
 		if (GetPrivateProfileIntA("Server", "PasswordHashed", 0, path.c_str()) != 1)
 		{
@@ -89,8 +93,8 @@ std::string wstring_to_string(const std::wstring& wstr)
 void fake_main(const char* path, int argc, wchar_t** argv)
 {
 	bool valid_args = false;
-	bool is_server  = false;
-	uint timeout    = 15000;
+	bool is_server = false;
+	uint timeout = 15000;
 
 	// This serves multiple purposes.
 	// e.g: If connecting, it stores a password for the remote server if applicable.
@@ -106,9 +110,9 @@ void fake_main(const char* path, int argc, wchar_t** argv)
 	{
 		if ((!wcscmp(argv[i], L"--host") || !wcscmp(argv[i], L"-h")) && i + 1 < argc)
 		{
-			address.port = std::stoi(argv[++i]);
-			valid_args   = true;
-			is_server    = true;
+			address.port = static_cast<sws::port_t>(std::stoi(argv[++i]));
+			valid_args = true;
+			is_server = true;
 		}
 		else if ((!wcscmp(argv[i], L"--connect") || !wcscmp(argv[i], L"-c")) && i + 1 < argc)
 		{
@@ -125,13 +129,13 @@ void fake_main(const char* path, int argc, wchar_t** argv)
 			const sws::port_t port = colon == std::string::npos ? 21790 : static_cast<sws::port_t>(std::stoi(ip.substr(colon + 1)));
 
 			address.address = ip.substr(0, colon);
-			address.port    = port;
-			valid_args      = true;
-			is_server       = false;
+			address.port = port;
+			valid_args = true;
+			is_server = false;
 		}
 		else if ((!wcscmp(argv[i], L"--timeout") || !wcscmp(argv[i], L"-t")) && i + 1 < argc)
 		{
-			timeout    = std::max(2500, std::stoi(argv[++i]));
+			timeout = std::max(2500, std::stoi(argv[++i]));
 			valid_args = true;
 		}
 		else if (!wcscmp(argv[i], L"--no-specials"))
@@ -141,7 +145,7 @@ void fake_main(const char* path, int argc, wchar_t** argv)
 		else if (!wcscmp(argv[i], L"--cheats"))
 		{
 			settings.cheats = true;
-			valid_args      = true;
+			valid_args = true;
 		}
 		else if (!wcscmp(argv[i], L"--password") && ++i < argc)
 		{
@@ -154,7 +158,7 @@ void fake_main(const char* path, int argc, wchar_t** argv)
 		else if (!wcscmp(argv[i], L"--local") || !wcscmp(argv[i], L"-l"))
 		{
 			settings.local = true;
-			valid_args     = true;
+			valid_args = true;
 		}
 		else if (!wcscmp(argv[i], L"--netstat"))
 		{
